5c30cc51401153cb3ed8b5813b63efa79e28bc48
dorianweb <37084529+dorianweb@users.noreply.github.com>
2020-01-10 10:07:01 +0100

    ajout du cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import routes from './routes'
 import InitDb from './db'
+import bodyParser  from 'body-parser'
+import cors  from 'cors'
+
+
 export default async () => {
     try {
         const app = express()
         await InitDb()
+        app.use(bodyParser.urlencoded({extended:true}))
+        app.use(cors())
         routes(app)
         app.listen(process.env.PORT, () => {
             console.log(process.env.PORT, process.env.NODE_ENV)
